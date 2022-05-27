50bc561b18eb7cbe2be02424da04f1e8b6fabdc4
Daniel Whitford <whitford.dsw@gmail.com>
2022-05-27 23:10:26 +0100

    portrait mode

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
-function setDateToNow(el) {
-    const now = new Date()
-    el.innerText = now.toLocaleTimeString()
+
+function pad(n) {
+    return ('0' + n).slice(-2)
 }
+
+function setDateToNow(h, m, s) {
+    const now = new Date()
+    h.innerText = pad( now.getHours() )
+    m.innerText = pad( now.getMinutes() )
+    s.innerText = pad ( now.getSeconds() )
+}
+
+const noSleep = new NoSleep();
 
 window.onload = function () {
     const container = document.getElementById('container')
-    const clockdiv = document.getElementById('clock')
+    const h = document.getElementById('hour')
+    const m = document.getElementById('min')
+    const s = document.getElementById('sec')
     const btn = document.getElementById('btn')
-    const noSleep = new NoSleep();
 
-    setDateToNow(clockdiv)
+    setDateToNow(h, m, s)
     setInterval(function () {
-        setDateToNow(clockdiv)
+        setDateToNow(h, m, s)
     }, 1000)
-
-    clockdiv.onclick = function () {
-        container.classList.replace("go", "rest")
-        setTimeout(function () {
-            container.classList.replace("rest", "go")
-        }, 2000)
-    }
 
     btn.onclick = function () {
         btn.remove()
